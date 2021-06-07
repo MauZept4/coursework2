303613d2dc5aa5ea584492438c2e5145bcbe0476
MauZept4 <mauzept4@gmail.com>
2021-06-07 22:12:56 +0300

    update fix

--- src/main/java/com/example/tourism/controller/AirOperatorRestController.java
+++ src/main/java/com/example/tourism/controller/AirOperatorRestController.java
@@ -16,31 +16,31 @@ public class AirOperatorRestController {
         this.service = service;
     }
 
-    @Operation(summary = "Get all activities")
+    @Operation(summary = "Get all air operators")
     @GetMapping("/get/all")
     public List<AirOperator> getAll(){
         return service.getAll();
     }
 
-    @Operation(summary = "Get activity by id")
+    @Operation(summary = "Get air operator by id")
     @GetMapping("/get/{id}")
-    public AirOperator getById(@PathVariable("id") @Parameter(name = "id", description = "ID of activity to get", required = true) String id){
+    public AirOperator getById(@PathVariable("id") @Parameter(name = "id", description = "ID of air operator to get", required = true) String id){
         return service.getById(id);
     }
 
-    @Operation(summary = "Delete activity by id")
+    @Operation(summary = "Delete air operator by id")
     @DeleteMapping("/delete/{id}")
     public AirOperator deleteById(@PathVariable("id") @Parameter(name = "id", description = "ID of activity to delete", required = true) String id){
         return service.delete(id);
     }
 
-    @Operation(summary = "Add new activity")
+    @Operation(summary = "Add new air operator")
     @PostMapping("/create/")
     public AirOperator create(@RequestBody AirOperator airOperator){
         return service.create(airOperator);
     }
 
-    @Operation(summary = "Edit activities")
+    @Operation(summary = "Edit air operator")
     @PutMapping("/update/")
     public AirOperator update(@RequestBody AirOperator airOperator){
         return service.update(airOperator);
